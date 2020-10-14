aecc0253f345c2bb1896b1c81f0a5694dbdb8cc1
JacksonBey <jacksongrowson@gmail.com>
2020-10-14 16:16:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+// function multiply(num1, num2) {
+//      num1 * num2
+// }
+
+// function random() {
+//     Math.floor(Math.random() * 10) + 1
+// }
+
+// function mod(){
+//     16 % 6 
+// }
+
+// function max(nums) {
+//     Math.max(nums)
+// }
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) + 1
+let mod = 16 % 6
+let array = [1,2,20]
+let max = Math.max(1,2,20)
